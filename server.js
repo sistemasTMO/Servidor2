f5e41eede2e7f28ae033fe845784f5c575a81753
sistemasTMO <sistemastmo@troqueladosmodulares.com.mx>
2025-01-25 08:12:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');  // Esto es necesario para usar promesas
-require('dotenv').config();
 
 const app = express();
 app.use(cors());
