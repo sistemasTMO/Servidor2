16cb13f4335f7453b96ee23dcdff6caa76d4b689
sistemasTMO <sistemastmo@troqueladosmodulares.com.mx>
2025-01-30 14:44:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 
-const app = express();
+const app = express(); 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
